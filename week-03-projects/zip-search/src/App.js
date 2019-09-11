f5185ab9572f94fa8f9c34306444726300295249
RyanLinrm <runminlin@gmail.com>
2019-09-11 14:54:25 -0400

    finished rendering city data in boostrap card

--- week-03-projects/zip-search/src/App.js
+++ week-03-projects/zip-search/src/App.js
@@ -1,9 +1,70 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from 'react-bootstrap/Card';
 
 
-function City(props) {
-  return (<div></div>);
+class City extends React.Component {
+  state = {
+    cities: [],
+    found: false,
+    data: []
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.zip === nextProps.zip && this.state.cities === nextState.cities) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  renderCity = async () => {
+    
+    const response = await fetch(`http://ctp-zip-api.herokuapp.com/zip/${this.props.zip}`);
+    
+    if(response.ok){
+
+      const data = await response.json();
+
+      let citylist = [];
+
+      for( let i = 0; i < data.length; i++){
+        let citydata = data[i]
+        citylist.push(citydata)
+      }
+    
+      let jaxOutList = citylist.map( (city) =>
+        <div key={city.RecordNumber}>
+          <Card style={{ width: '20rem', margin: 'auto' }}>
+            <Card.Header><b>{city.City}, {city.State}</b></Card.Header>
+            <Card.Body>
+              <ul>
+                <li>State: {city.State}</li>
+                <li>Location: ({city.Lat}, {city.Long})</li>
+                <li>Population(estimated): {city.EstimatedPopulation}</li>
+                <li>Total Wages: {city.TotalWages}</li>
+              </ul>
+            </Card.Body>
+          </Card>
+        </div>
+      )
+
+      this.setState({
+        cities: jaxOutList,
+        found: true
+      })
+  }
+
+  }
+
+  render(){
+
+    this.renderCity();
+    
+    return (<div>
+      {this.state.cities}
+    </div>);
+  }
 }
 
 class ZipSearchField extends React.Component {
@@ -24,7 +85,7 @@ class ZipSearchField extends React.Component {
   
   render(){
     return (<div className="text-center">
-      <label>Zip Code:</label>
+      <label><b>Zip Code:</b>&nbsp;</label>
       <input onChange={this.inputHandler} type="text"
          value={this.state.inputValue}></input>
     </div>);
@@ -39,11 +100,12 @@ class App extends Component {
   }
 
   getZipCodeHandler = (code) => {
+
     this.setState({
-      zipcode: code
+      zipcode: code,
+      showCities: true
     })
 
-    console.log(this.state.zipcode)
   }
 
   render() {
@@ -53,9 +115,8 @@ class App extends Component {
           <h2>Zip Code Search</h2>
         </div>
         <ZipSearchField getCode={this.getZipCodeHandler}/>
-        <div className="text-center">
-          <City />
-          <City />
+        <div>
+          <City zip={this.state.zipcode}/>
         </div>
       </div>
     );
